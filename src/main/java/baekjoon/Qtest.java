286d38cf94ab5d3acb26ad796d2db92cb66aa883
rockintuna <rockintuna@naver.com>
2021-08-27 15:58:11 +0900

    push

---
+++ src/main/java/baekjoon/Qtest.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Qtest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int val1 = scan.nextInt();
+        int val2 = scan.nextInt();
+        System.out.println(val1+val2);
+    }
+}
