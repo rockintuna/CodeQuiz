e094f9add4e72992e682c814c0021ab62c0eb610
rockintuna <rockintuna@naver.com>
2021-09-09 23:36:55 +0900

    Q2839

---
+++ src/main/java/baekjoon/Q2839.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2839 {
+
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static void main(String[] args) throws IOException {
+        Integer n = Integer.parseInt(reader.readLine());
+
+        if (n == 4 || n == 7) {
+            System.out.println(-1);
+        }
+        else if (n % 5 == 0) {
+            System.out.println(n / 5);
+        }
+        else if (n % 5 == 1 || n % 5 == 3) {
+            System.out.println((n / 5) + 1);
+        }
+        else if (n % 5 == 2 || n % 5 == 4) {
+            System.out.println((n / 5) + 2);
+        }
+    }
+}
