f25f530c6e2df78f238ed62a0463e4adb56aee32
rockintuna <rockintuna@naver.com>
2021-12-17 22:11:36 +0900

    CQ 211217

--- codeQuizWithPython/study/homework/homework4_2.py
+++ codeQuizWithPython/study/homework/homework4_2.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 current_r, current_c, current_d = 7, 4, 0
 current_room_map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -14,7 +16,34 @@ current_room_map = [
 ]
 
 def get_count_of_departments_cleaned_by_robot_vacuum(r, c, d, room_map):
-    return
+    directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+    queue = deque()
+    queue.append([r, c, d])
+    count = 1
+    room_map[r][c] = 2
+
+    while queue:
+        r, c, d = queue.popleft()
+        temp_d = d
+
+        for i in range(4):
+            temp_d = (temp_d + 3) % 4
+            temp_r = r + directions[temp_d][0]
+            temp_c = c + directions[temp_d][1]
+
+            if room_map[temp_r][temp_c] == 0:
+                room_map[temp_r][temp_c] = 2
+                queue.append([temp_r, temp_c, temp_d])
+                count += 1
+                break
+            elif i == 3:
+                temp_d = (d + 2) % 4
+                temp_r = r + directions[temp_d][0]
+                temp_c = c + directions[temp_d][1]
+                if room_map[temp_r][temp_c] == 1:
+                    return count
+                else:
+                    queue.append([temp_r, temp_c, d])
 
 # 57 가 출력되어야 합니다!
 print(get_count_of_departments_cleaned_by_robot_vacuum(current_r, current_c, current_d, current_room_map))
