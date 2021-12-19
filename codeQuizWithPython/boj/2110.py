0a69701878d9d10516f387ed12e183745247c54d
rockintuna <rockintuna@naver.com>
2021-12-20 01:28:01 +0900

    CQ 1220

--- codeQuizWithPython/boj/2110.py
+++ codeQuizWithPython/boj/2110.py
@@ -5,3 +5,24 @@ houses = []
 for _ in range(n):
     houses.append(int(sys.stdin.readline().rstrip()))
 
+houses.sort()
+upper_bound = houses[-1] - houses[0] + 1
+lower_bound = 1
+
+while upper_bound > lower_bound:
+    length = (upper_bound + lower_bound) // 2
+    count = 1
+    current_index = 0
+    for i in range(1, len(houses)):
+        if houses[i] - houses[current_index] >= length:
+            count += 1
+            current_index = i
+        else:
+            continue
+
+    if count >= c:
+        lower_bound = length + 1
+    elif count < c:
+        upper_bound = length
+
+print(upper_bound-1)
